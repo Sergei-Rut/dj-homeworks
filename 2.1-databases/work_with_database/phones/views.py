067ef57d96165e9af4d51fc6ddef4d04b0cc7fb1
Sergei Rut <ioga14@mail.ru>
2021-11-16 21:45:39 +0300

    2.1 commit

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,23 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    params = {
+        'name': 'name',
+        'min_price': 'price',
+        'max_price': '-price'
+    }
+    href_sort = request.GET.get('sort', 'name')
+    phone = Phone.objects.order_by(params.get(href_sort))
+    context = {
+        'phones': phone
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
